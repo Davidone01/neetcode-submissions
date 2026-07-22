ea6d56200815ee998352da0cc0d05d24839c6554
Davidone01 <100282047+Davidone01@users.noreply.github.com>
2026-07-22 17:58:48 +0200

    Add: capacity-to-ship-packages-within-d-days - submission-2

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def is_before(w):
+            days_count = 1
+            _sum = 0
+
+            for n in weights:
+                if _sum + n <=  w:
+                    _sum +=n
+                else:
+                    _sum = n
+                    days_count += 1
+            return days_count > days
+
+        if not weights:
+            return 0
+        l = max(weights) -1
+        r = sum(weights)
+
+        while (r-l)>1:
+            mid = (l+r)//2
+            if is_before(mid):
+                l = mid
+            else: 
+                r = mid
+        return r
